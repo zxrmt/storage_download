bd9c875b69450dfe61b2acf1d85436d329940068
zxrmt <49388235+zxrmt@users.noreply.github.com>
2024-01-26 15:54:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 import sqlite3
 import base64
-
+import sys
 
 headers = {
     'authority': 'www.googleapis.com',
@@ -22,10 +22,17 @@ headers = {
 }
 
 
+pageToken = "CjpsaW51eC1kZWJ1Zy9kOC1hcm0tYXNhbi1saW51eC1kZWJ1Zy12OC1jb21wb25lbnQtMzQxNjUuemlw"
+
+        
+
+
+
+
 conn = sqlite3.connect('v8_debug.db')
 cursor = conn.cursor()
 
-# Create a table if it does not exist
+# Print last commit
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS files (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -41,55 +48,121 @@ cursor.execute('''
 ''')
 
 
-pageToken = "CjpsaW51eC1kZWJ1Zy9kOC1hcm0tYXNhbi1saW51eC1kZWJ1Zy12OC1jb21wb25lbnQtMzQxNjUuemlw"
+if len(sys.argv) == 2:
+    if sys.argv[1] == "init":
 
-# GET PAGET TOKEN FROM DB
-try:
-    cursor.execute('SELECT nextPageToken FROM files ORDER BY id DESC LIMIT 1')
-    pageToken = cursor.fetchone()[0]
-except:
-    pass
+        # GET PAGET TOKEN FROM DB
+        try:
+            cursor.execute('SELECT nextPageToken FROM files ORDER BY id DESC LIMIT 1')
+            pageToken = cursor.fetchone()[0]
+        except:
+            pass
 
-print("pageToken: " + pageToken)
+        print("pageToken: " + pageToken)
 
-while True:
+        while True:
 
-    response = requests.get(
-        f'https://www.googleapis.com/storage/v1/b/v8-asan/o?delimiter=/&prefix=linux-debug/&fields=items(kind,mediaLink,metadata,name,size,updated),kind,prefixes,nextPageToken&pageToken={pageToken}',
-        headers=headers,
-    )
+            response = requests.get(
+                f'https://www.googleapis.com/storage/v1/b/v8-asan/o?delimiter=/&prefix=linux-debug/&fields=items(kind,mediaLink,metadata,name,size,updated),kind,prefixes,nextPageToken&pageToken={pageToken}',
+                headers=headers,
+            )
 
-    res = response.json()
-    if (not 'items' in res):
-        print("No more items")
-        break
+            res = response.json()
+            if (not 'items' in res):
+                print("No more items")
+                break
+            
+
+            for data in res['items']:   
+
+                # Create SQLite connection and cursor
+
+                # Insert data into the table
+                cursor.execute('''
+                    INSERT INTO files (nextPageToken, mediaLink, name, size, updated, cr_commit_position_number, cr_git_commit, cr_commit_position)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                ''', (
+                    pageToken,
+                    data['mediaLink'],
+                    data['name'],
+                    data['size'],
+                    data['updated'],
+                    data['metadata']['cr-commit-position-number'],
+                    data['metadata']['cr-git-commit'],
+                    data['metadata']['cr-commit-position']
+                ))
+
+                # Commit the changes and close the connection
+                conn.commit()
+                print("Inserted: " + data['name'], data['updated'], data['metadata']['cr-git-commit'])
+            
+            pageToken = res['nextPageToken']
+
+
+    else:
+        commit_id = sys.argv[1]
+        cursor.execute('''select * from files where cr_git_commit = ?''', (commit_id,))
+        rows = cursor.fetchall()
+        for row in rows:
+            # print(row[1],row[2])
+            print(row)
+else:
+    # Get last row from db 
+    cursor.execute('''select * from files order by cr_commit_position_number desc limit 1''')
+    rows = cursor.fetchall()
+    commit_id = rows[0][7]      
+    print(rows[0])
+    print("Last commit: " + commit_id       )
+    # Get all row with commit id
     
+    cursor.execute('''select * from files where cr_git_commit = ?''', (commit_id,))
+    rows = cursor.fetchall()
+    for row in rows:
+        # print(row[1],row[2])
+        print(row)
+        pageToken = row[1]  
+        last_posision = row[6]
 
-    for data in res['items']:   
+        while True:
+            print("pageToken Check: " + pageToken)
+            response = requests.get(
+                f'https://www.googleapis.com/storage/v1/b/v8-asan/o?delimiter=/&prefix=linux-debug/&fields=items(kind,mediaLink,metadata,name,size,updated),kind,prefixes,nextPageToken&pageToken={pageToken}',
+                headers=headers,
+            )
 
-        # Create SQLite connection and cursor
+            res = response.json()
+            if (not 'items' in res):
+                print("No more items")
+                break
 
-        # Insert data into the table
-        cursor.execute('''
-            INSERT INTO files (nextPageToken, mediaLink, name, size, updated, cr_commit_position_number, cr_git_commit, cr_commit_position)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (
-            pageToken,
-            data['mediaLink'],
-            data['name'],
-            data['size'],
-            data['updated'],
-            data['metadata']['cr-commit-position-number'],
-            data['metadata']['cr-git-commit'],
-            data['metadata']['cr-commit-position']
-        ))
+            if res['items'][0]['metadata']['cr-commit-position-number'] < last_posision:
+                print("No more items")
+                break
 
-        # Commit the changes and close the connection
-        conn.commit()
-        print("Inserted: " + data['name'], data['updated'], data['metadata']['cr-git-commit'])
-    
-    pageToken = res['nextPageToken']
 
+            for data in res['items']:   
+
+                # Insert data into the table
+                cursor.execute('''
+                    INSERT INTO files (nextPageToken, mediaLink, name, size, updated, cr_commit_position_number, cr_git_commit, cr_commit_position)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                ''', (
+                    pageToken,
+                    data['mediaLink'],
+                    data['name'],
+                    data['size'],
+                    data['updated'],
+                    data['metadata']['cr-commit-position-number'],
+                    data['metadata']['cr-git-commit'],
+                    data['metadata']['cr-commit-position']
+                ))
+
+                # Commit the changes and close the connection
+                conn.commit()
+                print("Inserted: " + data['name'], data['updated'], data['metadata']['cr-git-commit'])
+            
+            pageToken = res['nextPageToken'] 
+            last_posision = data['metadata']['cr-commit-position-number']
 
 conn.close()
-
+    
